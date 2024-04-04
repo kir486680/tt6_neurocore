3fed4d6a2db0a0dd8d1d289e8e5a201be1bb2fc5
Kyrylo Kalashnikov <kir486680@gmail.com>
2024-04-03 22:27:44 -0400

    uart is broken if I unitializ ui_in to 0

--- test/test.py
+++ test/test.py
@@ -22,13 +22,16 @@ async def test_project(dut):
     # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
-    #dut.ui_in.value = 0
+    dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
     await ClockCycles(dut.clk, 10)
-
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 10)
+    dut.rst_n.value = 1
+    await ClockCycles(dut.clk, 10)
     # Case 1: ui_in is 1, expect uo_out to be all 1's
     # dut._log.info("Test with ui_in = 1")
     # dut.ui_in.value = 1  # Set ui_in to 1
